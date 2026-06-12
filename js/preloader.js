955b2a9db553db5ea41f936b2b67aecf357b81be
Seu Nome <moisesvfgouveia@gmail.com>
2026-06-12 13:53:00 -0300

    Remove all preloader animations - static display

--- js/preloader.js
+++ js/preloader.js
@@ -1,6 +1,5 @@
 window.addEventListener('load', () => {
   const preloader = document.getElementById('preloader');
-  const introLogo = document.getElementById('preloader-logo');
   const navLogo = document.querySelector('.site-logo img');
 
   if (!navLogo) {
@@ -9,15 +8,7 @@ window.addEventListener('load', () => {
   }
 
   gsap.set(navLogo, { autoAlpha: 0 });
-
-  const tl = gsap.timeline({
-    onComplete: () => {
-      gsap.set(navLogo, { autoAlpha: 1 });
-      preloader.remove();
-    }
-  });
-
-  tl.to(introLogo, { scale: 1.15, duration: 0.6, ease: 'power2.out' }, 0)
-    .to(introLogo, { scale: 1, duration: 0.5, ease: 'power2.inOut' }, 0.6)
-    .to(preloader, { backgroundColor: 'rgba(8, 25, 45, 0)', duration: 0.8 }, 0.8);
+  gsap.set(preloader, { backgroundColor: 'rgba(8, 25, 45, 0)' }, 0);
+  gsap.set(navLogo, { autoAlpha: 1 });
+  preloader.remove();
 });
